d4f646042bbb560ab5aa1150dcb151cc00593ef6
kaixa <690750353@qq.com>
2020-03-07 21:12:26 +0800

    小小修改设置文件

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,4 @@
 import * as vscode from 'vscode';
-import * as fs from 'fs';
-import { execFile } from 'child_process';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,17 +7,21 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "masm-code" is now active!');
+	const DosBoxPath = vscode.workspace.getConfiguration('masm-code.DOSBox').get('path') as string;
+	console.log(context.globalStoragePath);
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let runInBox = vscode.commands.registerCommand('extension.runInBox', (param) => {
-		vscode.languages.getLanguages().then(s => console.log(s));
-		vscode.window.createTerminal('DOSBox', 'C:/Users/RTkaixa/Desktop/masm-code/dosbox/DOSBox.exe');
+		console.log(DosBoxPath);
+		if (DosBoxPath !== null) {
+			vscode.window.createTerminal('DOSBox', DosBoxPath);
+		} else {
+			vscode.window.showErrorMessage('未设置DOSBox路径。');
+		}
 	});
-
 	context.subscriptions.push(runInBox);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() { }
